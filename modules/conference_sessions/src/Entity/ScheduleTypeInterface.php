b251c82859f08e848466258cb83fbeb811a8d8bc
Matt Glaman <nmd.matt@gmail.com>
2016-03-31 22:07:40 -0500

    Add module

--- modules/conference_sessions/src/Entity/ScheduleTypeInterface.php
+++ modules/conference_sessions/src/Entity/ScheduleTypeInterface.php
@@ -3,7 +3,7 @@
 namespace Drupal\conference_sessions\Entity;
 
 use Drupal\Core\Config\Entity\ConfigEntityInterface;
-use Drupal\entity\Entity\EntityDescriptionInterface;
+use Drupal\Core\Entity\EntityDescriptionInterface;
 
 /**
  * Defines interface for session room types.
