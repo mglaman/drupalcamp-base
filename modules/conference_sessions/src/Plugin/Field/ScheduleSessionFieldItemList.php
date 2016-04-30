56d440247c455cdfb7994a3a431974ef27469210
Matt Glaman <nmd.matt@gmail.com>
2016-04-29 22:28:19 -0500

    Add schedule session item field

---
+++ modules/conference_sessions/src/Plugin/Field/ScheduleSessionFieldItemList.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\conference_sessions\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\EntityReferenceFieldItemList;
+
+class ScheduleSessionFieldItemList extends EntityReferenceFieldItemList {
+  
+}
---
+++ modules/conference_sessions/src/Plugin/Field/ScheduleSessionItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\conference_sessions\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+use Drupal\Core\TypedData\DataDefinition;
+
+class ScheduleSessionItem extends EntityReferenceItem {
+
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties =  parent::propertyDefinitions($field_definition);
+
+    $properties[';value'] = DataDefinition::create('datetime_iso8601')
+      ->setLabel(t('Date value'))
+      ->setRequired(TRUE);
+
+    $properties['date'] = DataDefinition::create('any')
+      ->setLabel(t('Computed date'))
+      ->setDescription(t('The computed DateTime object.'))
+      ->setComputed(TRUE)
+      ->setClass('\Drupal\datetime\DateTimeComputed')
+      ->setSetting('date source', 'value');
+
+    return $properties;
+  }
+
+}
